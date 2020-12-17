e7f41a5fe02d02b8ec24a813ad1e664144923471
Jake Koperski <wijakoperski@gmail.com>
2020-12-17 07:36:34 -0600

    Enabled Netlify deploy

--- prettier.config.js
+++ prettier.config.js
@@ -1,17 +1,17 @@
 // https://prettier.io/docs/en/options.html
 module.exports = {
-  trailingComma: 'es5',
-  bracketSpacing: true,
-  tabWidth: 2,
-  semi: false,
-  singleQuote: true,
-  arrowParens: 'always',
-  overrides: [
-    {
-      files: 'Routes.js',
-      options: {
-        printWidth: 200,
-      },
-    },
-  ],
-}
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    arrowParens: 'always',
+    overrides: [
+        {
+            files: 'Routes.js',
+            options: {
+                printWidth: 200,
+            },
+        },
+    ],
+};
